8e2c838f28b439af2cea7ae8ebd570f42ec5cf87
Tim Kilbourn <tkilbourn@google.com>
2016-09-19 10:45:43 -0700

    [osboot] Refactor magenta loading code out of osboot
    Change-Id: I3ca5d46ed0e864f712c1cc7c27999394222b72c5

---
+++ src/magenta.h
@@ -0,0 +1,16 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <efi.h>
+
+typedef struct {
+    UINT8* zeropage;
+    UINT8* cmdline;
+    void* image;
+    UINT32 pages;
+} kernel_t;
+
+int boot_kernel(EFI_HANDLE img, EFI_SYSTEM_TABLE* sys,
+                void* image, size_t sz, void* ramdisk, size_t rsz,
+                void* cmdline, size_t csz);
